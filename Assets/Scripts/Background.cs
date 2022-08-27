0b4fbc0224332f4e265596ac4b1a770f75a1d8e7
MyeungJunLee <audwns356@gmail.com>
2022-08-28 02:37:43 +0900

    no message

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField] GameObject target = null;
+
+    private void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, transform.position.z);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (LogicManager.Inst != null)
+        {
+            LogicManager.Inst.playerObj.GetComponent<PlayerMovement>().Die();
+        }
+    }
+}
