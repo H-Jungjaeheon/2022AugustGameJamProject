4c373921e17a48bc890639ac22730cad4c319aa4
WiFros <bjy123bjy@gmail.com>
2022-08-28 03:23:10 +0900

    Test

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    Rigidbody2D rigid;
+    private void Update()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Vector3 dir = LogicManager.Inst.playerObj.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rigid.rotation = angle;
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
