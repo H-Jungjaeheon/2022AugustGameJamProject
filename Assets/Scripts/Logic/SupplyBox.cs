8ee34b55da3947073c168cc16586292049525b86
H-JungJaeHeon <jjhking222@naver.com>
2022-08-28 10:52:18 +0900

    BoxRandResourceSystemMake
    BoxRandResourceSystemMake

--- Assets/Scripts/Logic/SupplyBox.cs
+++ Assets/Scripts/Logic/SupplyBox.cs
@@ -4,20 +4,50 @@ using UnityEngine;
 
 public class SupplyBox : MonoBehaviour
 {
+    [SerializeField]
+    private Sprite[] sprites;
+
+    [SerializeField]
+    private Vector3[] objScale;
+
+    [SerializeField]
+    private bool isRandResourceChange;
+
+    SpriteRenderer SR;
+
+    void Awake()
+    {
+        RandResource();
+    }
+
+    private void RandResource()
+    {
+        if (isRandResourceChange == false) return;
+
+        SR = GetComponent<SpriteRenderer>();
+        int resourceIndex = Random.Range(0, 3);
+        SR.sprite = sprites[resourceIndex];
+        transform.localScale = objScale[resourceIndex];
+        //switch (resourceIndex)
+        //{
+        //    case 0:
+        //        SR.sprite = sprites[0];
+        //        break;
+        //    case 1:
+
+        //        break;
+        //    case 2:
+
+        //        break;
+        //}
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (collision.gameObject.CompareTag("SupplyBoxDestryZone"))
-        //{
-        //    SupplyBoxSpawner.Inst.NowSupplyBoxCount--;
-        //    if(GetComponent<PooledObject>())
-        //        GetComponent<PooledObject>().Pool.ReturnObject(gameObject);
-        //    else
-        //        Destroy(gameObject);
-        //}
         if (collision.gameObject.CompareTag("SupplyBoxDestryZone"))
         {
             SupplyBoxSpawner.Inst.NowSupplyBoxCount--;
-            gameObject.SetActive(false);
+            Destroy(gameObject);
         }
     }
 }
